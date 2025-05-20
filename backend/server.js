7bd7552616d57f375c3ec5624adf76040aff258c
naman005 <namanchaturvedi05@gmail.com>
2025-05-20 09:49:31 +0530

    Use process.env.PORT in server.js

--- backend/server.js
+++ backend/server.js
@@ -36,5 +36,5 @@ app.use("/api/v1/expense", expenseRoutes);
 app.use("/api/v1/dashboard", dashboardRoutes);
 
 // Server start
-const PORT = 8000;
+const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
--- frontend/src/utlis/apiPaths.js
+++ frontend/src/utlis/apiPaths.js
@@ -1,5 +1,5 @@
 // Base API URL
-export const BASE_URL = "http://localhost:8000";
+export const BASE_URL = process.env.REACT_APP_API_URL;
 
 export const API_PATHS = {
     AUTH: {
